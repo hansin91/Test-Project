976852e61a82821d93ddf31736128e8635c8abed
Hansin Susatya <hansinsusatya@gmail.com>
2018-01-23 13:42:27 +0700

    Add test to express app

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send('hello'));
+app.get('/test, (req, res) => res.send('test'));
 
 app.listen(3000, () => console.log('hello'));
