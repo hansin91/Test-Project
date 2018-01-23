15dab6307d377422c653433ced7bc7d47ecc34da
Randy W <randywenas@gmail.com>
2018-01-23 17:52:02 +0700

    Fixing bugs

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => res.send('hello'));
-app.get('/test, (req, res) => res.send('test'));
+app.get("/", (req, res) => res.send("hello"));
 
-app.listen(3000, () => console.log('hello'));
+app.listen(3000, () => console.log("hello"));
